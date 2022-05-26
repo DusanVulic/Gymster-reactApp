4b05f4516079274e892a84ff2df63e2cdf12baa2
Dusan <dusanvulic11@gmail.com>
2022-05-26 12:59:39 +0200

    firebase , context and hooks setup finished

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Gymster react app</div>;
+  return (
+    <div className="App">
+      <h3 className="title">Gymster react application</h3>
+    </div>
+  );
 }
 
 export default App;
