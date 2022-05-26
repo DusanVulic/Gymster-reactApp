4b2a32e6816236d5d8d7ed0160171b9924bac731
Dusan <dusanvulic11@gmail.com>
2022-05-26 13:41:39 +0200

    routes setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+// PAGES
+
+import Dashborad from "./components/dashboard/Dashborad";
+import Login from "./components/login/Login";
+import Create from "./components/create/Create";
+import Project from "./components/project/Project";
+import Signup from "./components/signup/Signup";
+
 function App() {
   return (
     <div className="App">
-      <h3 className="title">Gymster react application</h3>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route exact path="/">
+              <Dashborad />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/projects/:id">
+              <Project />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/signup">
+              <Signup />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
