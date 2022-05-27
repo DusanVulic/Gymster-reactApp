ee8bb7630822a4f1bd7b4203f067784f75b7c127
Dusan <dusanvulic11@gmail.com>
2022-05-27 12:24:29 +0200

    signup page created[C

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "./Signup.module.css";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [thumbnail, setThumbnail] = useState("");
+
+  return (
+    <form className={styles.form}>
+      <h3>signup</h3>
+      <label>
+        <span>email:</span>
+        <input
+          required
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>password:</span>
+        <input
+          required
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>display name:</span>
+        <input
+          required
+          type="text"
+          value={displayName}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+      </label>
+      <label>
+        <span>profile image:</span>
+        <input
+          required
+          type="file"
+          value={thumbnail}
+          onChange={(e) => setThumbnail(e.target.value)}
+        />
+      </label>
+    </form>
+  );
 };
 
 export default Signup;
