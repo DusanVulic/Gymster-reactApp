de43b5580d755f5c029baaf6cf72fbe619232926
Dusan <dusanvulic11@gmail.com>
2022-05-31 12:50:33 +0200

    making a project list

--- src/components/WorkoutList.js
+++ src/components/WorkoutList.js
@@ -3,12 +3,30 @@ import React from "react";
 //import styles
 import styles from "./WorkoutList.module.css";
 
+//LINK
+
+import { Link } from "react-router-dom";
+//import avatar component
+import Avatar from "./Avatar";
+
 const WorkoutList = ({ workouts }) => {
   return (
-    <div>
+    <div className={styles.workout}>
       {workouts.length === 0 && <p>No workouts yet ! </p>}
       {workouts.map((workout) => (
-        <div key={workout.id}>{workout.name}</div>
+        <Link key={workout.id} to={`projects/${workout.id}`}>
+          <h4>{workout.name}</h4>
+          <p> due by {workout.dueDate.toDate().toDateString()}</p>
+          <div className={styles.assigned}>
+            <ul>
+              {workout.assignedUSersList.map((user) => (
+                <li key={user.id}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
       ))}
     </div>
   );
--- src/pages/dashboard/Dashborad.js
+++ src/pages/dashboard/Dashborad.js
@@ -11,7 +11,7 @@ const Dashborad = () => {
   const { documents, error } = useCollection("workouts");
   return (
     <div>
-      <h2 className={styles.title}> Dashborad</h2>
+      <h2 className={styles.title}> Dashboard</h2>
       {error && <p className="error"> oooops....something goes wrong</p>}
       {documents && <WorkoutList workouts={documents} />}
     </div>
